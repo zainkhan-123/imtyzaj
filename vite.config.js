d06e2da5b2e61ec8ad86c16beabebd239e3b1556
zain iqbal <zainkhanii646@gmail.com>
2025-03-08 12:38:28 +0500

    Update PWA configuration and rebuild for manually cache version upadate

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,26 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [react(),
   VitePWA({
+    strategies: 'generateSW', // ya 'injectManifest', jo aap use kar rahe hain
+      workbox: {
+        // Purani caches ko cleanup karne ke liye
+        cleanupOutdatedCaches: true,
+        runtimeCaching: [
+          {
+            urlPattern: /\.(png|jpg|jpeg|svg)$/,
+            handler: 'CacheFirst',
+            options: {
+              // Yahan cache name mein version update kar sakte hain
+              cacheName: 'my-app-images-v2',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+              },
+            },
+          },
+        ],
+      },
+
     registerType: 'autoUpdate', // Service Worker ko auto update karne ke liye
     includeAssets:['kaaba.png',
       'logo.png'],
@@ -28,6 +48,11 @@ export default defineConfig({
         {
           src: '/logo2.png',
           sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: '.png',
+          sizes: '192x192',
           type: 'image/png'
         }
         // Agar aapke paas additional icon sizes hain, unko yahan add karein
