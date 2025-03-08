81ad888cbe4122b91ab11fbb565a68679b15c512
zain iqbal <zainkhanii646@gmail.com>
2025-03-08 10:01:21 +0500

    Update: PWA configuration and updated build for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), ],
+  plugins: [react(), 
+    VitePWA({
+      registerType: 'autoUpdate', // Service Worker ko auto update karne ke liye
+      includeAssets: ['/kaaba.jpg', '/logo.png' ],
+      
+      manifest: {
+        name: 'ابوبکر روحانی ویلفیرٹرسٹ',  // Full app name
+        short_name: 'روحانی',              // Short name for home screen
+        description: 'یہ ایک Progressive Web App ہے jo Urdu mein bana hai.', // Description
+        theme_color: '#ffffff',            // Theme color for browser UI
+        background_color: '#ffffff',       // Background color on launch
+        start_url: '/',                    // Start URL when launched
+        display: 'standalone',             // Display mode
+        icons: [
+          {
+            src: '/logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/logo2.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+          // Agar aapke paas additional icon sizes hain, unko yahan add karein
+        ]
+      }
+    })
+
+  ],
   
 })
